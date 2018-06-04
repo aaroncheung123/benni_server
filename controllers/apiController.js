d8df267ea81676717896e16ab643158b4a9fff35
Aaron Cheung <aaroncheung123@gmail.com>
2018-06-04 15:37:51 -0600

    t1

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -6,6 +6,7 @@ module.exports = function(app){
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  //RETRIEVE INFORMATION BY ALL
   app.get('/api/todos/:username', function(req, res){
     Todos.find({ username: req.params.username},
     function(err,todos){
@@ -14,6 +15,7 @@ module.exports = function(app){
     });
   });
 
+  //RETRIEVE INFORMATION BY ID
   app.get('/api/todo/:id', function(req, res){
     Todos.findById({ _id: req.params.id},
     function(err,todos){
@@ -22,13 +24,12 @@ module.exports = function(app){
     });
   });
 
-
+  //UPDATE
   app.post('/api/todo', function(req, res){
     if(req.body.id){
       Todos.findByIdAndUpdate(req.body.id,{
         todo:req.body.todo, isDone: req.body.isDone, hasAttachment: req.body.hasAttachment}, function(err, todo){
           if(err) throw err;
-
           res.send('Success');
         });
       }
@@ -46,6 +47,8 @@ module.exports = function(app){
     }
   });
 
+
+  //DELETE
   app.delete('/api/todo', function(req, res){
     Todos.findByIdAndRemove(req.body.id,
     function(err,todos){
