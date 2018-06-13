26f6508ebf1776a488676221a5f1286bc57d553e
Aaron Cheung <aaroncheung123@gmail.com>
2018-06-13 13:40:50 -0600

    Right before changing mongoose schema

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var http = require('http').Server(app);
 
 var io = require('socket.io')(http);
 app.get('/',function(req,res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');git 
 })
 io.on('connection', function(socket){
   console.log('one user connected ' + socket.id);
