8acf150c35b8651a0cbd5c0c845c4c061c7874b1
Aaron Cheung <aaroncheung123@gmail.com>
2018-06-13 14:24:36 -0600

    Testing user data schema

--- models/userDataModel.js
+++ models/userDataModel.js
@@ -2,13 +2,37 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
+// var userDataSchema = new Schema({
+//     info:{
+//         firstName: String,
+//         lastName: String,
+//         phone: String,
+//         address: String,
+//         email: String,
+//         password: String
+//     },
+//     progressNumbers:{
+//         drive: Number,
+//         chat: Number,
+//         math: Number,
+//         charge: Number
+//     }
+// });
+
 var userDataSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    phone: String,
-    address: String,
-    email: String,
-    password: String
+        firstName: String,
+        lastName: String,
+        phone: String,
+        address: String,
+        email: String,
+        password: String,
+        num: Number,
+        progressNumbers:{
+        drive: Number,
+        chat: Number,
+        math: Number,
+        charge: Number
+    }
 });
 
 var userData = mongoose.model('userData', userDataSchema);
